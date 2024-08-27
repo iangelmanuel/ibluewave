7fdd1907315982f635b31c7874a9fd8daa783621
iAngelManuel <iangelmanueldm@gmail.com>
2024-08-27 18:43:51 -0500

    chore: Update admin model schema

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -1,30 +1,33 @@
-import { Schema, model, models } from 'mongoose'
-import bcrypt from 'bcryptjs'
-import type { IAdmin } from '@/types/main'
+import { Schema, model, models } from "mongoose"
+import bcryptjs from "bcryptjs"
+import type { IAdmin } from "@/types/main"
 
-const adminSchema = new Schema<IAdmin>({
-  name: {
-    type: String,
-    required: true,
-    trim: true
+const adminSchema = new Schema<IAdmin>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
+  {
+    timestamps: true,
+    versionKey: false
   }
-}, {
-  timestamps: true,
-  versionKey: false
-})
+)
 
 adminSchema.methods.comparePassword = async function (passwordForm: string) {
-  return await bcrypt.compare(passwordForm, this.password)
+  return await bcryptjs.compare(passwordForm, this.password)
 }
 
-const Admin = models.Admin ?? model('Admin', adminSchema)
+const Admin = models.Admin ?? model("Admin", adminSchema)
 export default Admin
